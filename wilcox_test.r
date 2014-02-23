1d8969d9562c1ad7b03b8fbad3c76998be905ae6
Adam <stelmaszczykadam@gmail.com>
2014-02-23 11:38:41 +0100

    Wilcoxon test writes to files and dooesn't need arguments

--- wilcox_test.r
+++ wilcox_test.r
@@ -1,30 +1,32 @@
 #!/usr/bin/Rscript
 
-dim = commandArgs(TRUE)[1]
-basepath = paste("ready/", dim, "/", sep = "")
-infopath = paste(basepath, "demid1_", dim, "D", sep = "")
-
-for (file in dir(basepath))
+for (dim in c(10, 20, 40, 80))
 {
-	filepath = paste(basepath, file, sep = "")
-	if (file.info(filepath)$isdir)
+	basepath = paste("ready/", dim, "/", sep = "")
+	infopath = paste(basepath, "demid1_", dim, "D", sep = "")
+	outfile = paste("wilcox_test/", dim, sep = "")
+	for (file in dir(basepath))
 	{
-		for (infofile in dir(infopath, pattern = "info"))
+		filepath = paste(basepath, file, sep = "")
+		if (file.info(filepath)$isdir)
 		{
-			data1 = scan(paste(infopath, "/", infofile, sep = ""), quiet = TRUE)
-			data2 = scan(paste(filepath, "/", infofile, sep = ""), quiet = TRUE)
-			test = wilcox.test(data1, data2, exact = FALSE)
-			if (test$p.value < 0.05)
+			for (infofile in dir(infopath, pattern = "info"))
 			{
-				if (mean(data1) < mean(data2))
-					cat("+")
-				else
-					cat("-")
-			} else 
-				cat(".")
+				data1 = scan(paste(infopath, "/", infofile, sep = ""), quiet = TRUE)
+				data2 = scan(paste(filepath, "/", infofile, sep = ""), quiet = TRUE)
+				test = wilcox.test(data1, data2, exact = FALSE)
+				if (test$p.value < 0.05)
+				{
+					if (mean(data1) < mean(data2))
+						cat("+", file = outfile, append = TRUE)
+					else
+						cat("-", file = outfile, append = TRUE)
+				} else 
+					cat(".", file = outfile, append = TRUE)
+			}
+			cat(" ", file = outfile, append = TRUE)
+			cat(file, file = outfile, append = TRUE)
+			cat("\n", file = outfile, append = TRUE)
 		}
-		cat(" ")
-		cat(file)
-		cat("\n")
 	}
 }
