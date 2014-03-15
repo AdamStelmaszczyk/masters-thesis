718278392c74c226824511ff451b414c7fbfb042
Adam <stelmaszczykadam@gmail.com>
2014-03-15 16:55:49 +0100

    Wilcoxon test results

--- wilcox_test.r
+++ wilcox_test.r
@@ -1,32 +1,40 @@
 #!/usr/bin/Rscript
 
+algorithm = "debest1"
+outfile = paste("wilcox_test/", algorithm, sep = "")
+cat(file = outfile)
 for (dim in c(10, 20, 40, 80))
 {
 	basepath = paste("ready/", dim, "/", sep = "")
-	infopath = paste(basepath, "demid1_", dim, "D", sep = "")
-	outfile = paste("wilcox_test/", dim, sep = "")
+	infopath = paste(basepath, algorithm, "_", dim, "D", sep = "")
 	for (file in dir(basepath))
 	{
 		filepath = paste(basepath, file, sep = "")
 		if (file.info(filepath)$isdir)
 		{
+			name = sub("de", "DE/", file)
+			name = sub("_.*", "", name)
+			name = sub("(rand|mid|best)", "\\1/", name)
+			name = sub("inf", "$\\\\infty$", name)
+			cat(name, file = outfile, append = TRUE)
+			cat("\t", file = outfile, append = TRUE)
 			for (infofile in dir(infopath, pattern = "info"))
 			{
 				data1 = scan(paste(infopath, "/", infofile, sep = ""), quiet = TRUE)
 				data2 = scan(paste(filepath, "/", infofile, sep = ""), quiet = TRUE)
 				test = wilcox.test(data1, data2, exact = FALSE)
+				cat(" & ", file = outfile, append = TRUE)
 				if (test$p.value < 0.05)
 				{
 					if (mean(data1) < mean(data2))
 						cat("+", file = outfile, append = TRUE)
 					else
-						cat("-", file = outfile, append = TRUE)
+						cat("--", file = outfile, append = TRUE)
 				} else 
-					cat(".", file = outfile, append = TRUE)
+					cat("$\\cdot$", file = outfile, append = TRUE)				
 			}
-			cat(" ", file = outfile, append = TRUE)
-			cat(file, file = outfile, append = TRUE)
-			cat("\n", file = outfile, append = TRUE)
+			cat(" \\\\\n", file = outfile, append = TRUE)
 		}
 	}
+	cat("\n", file = outfile, append = TRUE)
 }
