cbb9522ddaedb075e19a97bbc55d3dc0bfa68903
Adam <stelmaszczykadam@gmail.com>
2014-03-30 17:34:36 +0200

    Doc update

--- png.r
+++ png.r
@@ -17,16 +17,21 @@ start = regexpr("f", filename[1], fixed=T)[1] + 1
 end = regexpr(".", filename[1], fixed=T)[1] - 1
 fnum = substr(filename[1], start, end)
 
-names = rep(NA, args)
-for (i in 1:args)
-	names[i] = substr(filename[i], 0, regexpr("/", filename[i], fixed=T)[1] - 1)
+names = c(
+"DE/rand/1", "DE/rand/2", "DE/rand/6", "DE/rand/∞", 
+"DE/best/1", "DE/best/2", "DE/best/6", "DE/best/∞", 
+"DE/mid/1", "DE/mid/2", "DE/mid/6", "DE/mid/∞")
 	
 png(paste("pngs/", fnum, ".png", sep = ""))
 
-colors = c("springgreen", "yellowgreen", "palegreen4", "black", "cadetblue1", "blue", "blueviolet", "darkblue", "orange", "salmon", "magenta", "red" )
+colors = c("black", "red", "green", "blue")
 
-plot(ecdf(data[[1]]), verticals = TRUE, do.points = FALSE, col = colors[1], main = fnum, xlab = "najlepszy - minimum", ylab = "Prawdopodobieństwo", xlim = c(xmin, xmax), ylim = c(0, 1))
-for (i in 2:args)
-	lines(ecdf(data[[i]]), verticals = TRUE, do.points = FALSE, col = colors[i])
+plot(ecdf(data[[1]]), verticals = TRUE, do.points = FALSE, col = colors[1], main = fnum, xlab = "najlepszy - minimum", ylab = "Prawdopodobieństwo", xlim = c(xmin, xmax), ylim = c(0, 1), lwd = 2)
+for (i in 2:4)
+	lines(ecdf(data[[i]]), verticals = TRUE, do.points = FALSE, col = colors[i], lwd = 2)
+for (i in 5:8)
+	lines(ecdf(data[[i]]), verticals = TRUE, do.points = FALSE, col = colors[i - 4], lty = 2, lwd = 2)
+for (i in 9:12)
+	lines(ecdf(data[[i]]), verticals = TRUE, do.points = FALSE, col = colors[i - 8], lty = 3, lwd = 2)
 	
-legend("bottomright", names, col = colors, lty = 1);
+legend("bottomright", names, col = colors, bty="n", lty = c(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3), lwd = 2);
